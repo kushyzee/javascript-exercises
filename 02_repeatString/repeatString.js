524b62e855caef404643f8faa0d87c2e73ec5418
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-01-31 00:49:43 +0100

    write a function that satisfy all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number < 0) return 'ERROR'
+  let repeatedString = ''
+  for (let i = 0; i < number; i++) {
+    repeatedString += string;
+  }
+  return repeatedString
 };
 
+
+console.log(repeatString("hey", -1))
 // Do not edit below this line
 module.exports = repeatString;
