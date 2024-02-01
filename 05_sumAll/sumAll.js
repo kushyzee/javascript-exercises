dd7f80137ce6f8cf84277583786993effa3462eb
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-01 13:27:53 +0100

    Implement a function that passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  if (typeof firstNum !== 'number' || typeof secondNum !== 'number' || firstNum < 0 || secondNum < 0) {
+    return "ERROR"
+  }
+
+  sumTotal = 0
+  if (firstNum < secondNum) {
+    for (let i = firstNum; i <= secondNum; i++) {
+      sumTotal += i;
+    }
+    return sumTotal;
+  }
+
+  else if (firstNum > secondNum) {
+    for (let i = secondNum; i <= firstNum; i++) {
+      sumTotal += i
+    }
+    return sumTotal
+  }
 
 };
 
+let result = sumAll(4, '1');
+console.log(result)
 // Do not edit below this line
 module.exports = sumAll;
