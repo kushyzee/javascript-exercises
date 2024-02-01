34b95b3230392f293e8fc63b794383783dd434cd
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-01 12:05:46 +0100

    remove elements from an array using map and splice methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...numArr) {
+  numArr.map((num) => {
+    let currentIndex = arr.indexOf(num);
+    if (currentIndex >= 0) arr.splice(currentIndex, 1);
+  });
+  return arr;
 };
+
+console.log(removeFromArray([1, 2, 3], '1', 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
