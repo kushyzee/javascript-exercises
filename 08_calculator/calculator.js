0726de039adda1b28c8322be80f3835f0368503a
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-23 12:58:40 +0100

    Write function to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  return numbers.reduce((acc, number) => acc + number, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  return numbers.reduce((acc, number) => acc - number)
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  return numArray.reduce((acc, number) => acc + number, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((acc, number) => acc * number, 1)
 };
 
-const power = function() {
-	
+const power = function(num, numPower) {
+	let result = num;
+  for(let i = 0; i <= numPower - 2; i++) {
+    result = result * num
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+
+  let result = 1
+  for (let i = 1; i < num; i++) {
+    result = result * (i + 1)
+  }
+  return result
 };
+
+const result = factorial(5)
+console.log(result)
 
 // Do not edit below this line
 module.exports = {
